c039bb7eca2ad4fb72224f19d11b86aa6e94578e
Andrew Dias <andrew-dias@users.noreply.github.com>
2016-09-23 23:22:47 -0400

    Use zsh-nvm plugin for nvm

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,8 @@
+# NVM
+export NVM_DIR=$HOME/.local/share/nvm
+#export NVM_LAZY_LOAD=true
+source $HOME/.config/zsh/plugins/zsh-nvm/zsh-nvm.plugin.zsh
+
 # ZSH syntax highlighting
 source $HOME/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
