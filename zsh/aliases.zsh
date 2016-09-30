448364fa22ef10167ed7ca2c2654928824e1d71d
Andrew Dias <andrew-dias@users.noreply.github.com>
2016-09-30 18:59:13 -0400

    Add zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,12 @@
+# Use colors in coreutils utilities output
 alias ls='ls --color=auto'
+alias grep='grep --color'
+
+# ls aliases
+alias la='ls -la'
+alias l='ls'
+
+# Aliases to protect against overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+
