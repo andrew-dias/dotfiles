5217c25968f2c1740ed73fbde97ec8d999bdafef
Andrew Dias <andrew-dias@users.noreply.github.com>
2017-12-19 14:19:58 -0500

    Add Oracle Instant Client env vars

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,15 @@
-#JAVA_HOME
+# JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 # add local binaries to PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.yarn/bin:$PATH
+
+# Oracle Instant Client
+# https://help.ubuntu.com/community/Oracle%20Instant%20Client
+ORACLE_VERSION=12.2
+export ORACLE_HOME=/usr/lib/oracle/$ORACLE_VERSION/client64
+export PATH=$PATH:$ORACLE_HOME/bin
+export LD_LIBRARY_PATH=/usr/lib/oracle/$ORACLE_VERSION/client64/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export TNS_ADMIN=/home/andrew/setup
 
